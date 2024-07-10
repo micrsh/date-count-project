50a42a7a47602415238876ea9248a3505dce8580
Micrsh <Meling240399@gmail.com>
2024-07-10 22:14:56 +0700

    Updated CouterV2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <Counter />
+      <CounterV2 />
     </div>
   );
 }
@@ -53,6 +54,68 @@ function Counter() {
           Today is <span>{date.toDateString()}</span>
         </p>
       )}
+    </div>
+  );
+}
+
+function CounterV2() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleStepChange(e) {
+    setStep(+e.target.value);
+  }
+
+  function handleCountChange(e) {
+    setCount(+e.target.value);
+  }
+
+  function handleCountPlus() {
+    setCount((c) => count + step);
+  }
+
+  function handleCountMinus() {
+    setCount((c) => count - step);
+  }
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
+  return (
+    <div className="CounterV2">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={handleStepChange}
+        />
+        <span>Step:{step}</span>
+      </div>
+      <div>
+        <button onClick={handleCountMinus}>-</button>
+        <input type="text" value={count} onChange={handleCountChange} />
+        <button onClick={handleCountPlus}>+</button>
+      </div>
+      <div>
+        {count == 0 ? (
+          <p>
+            Today is <span>{date.toDateString()}</span>
+          </p>
+        ) : (
+          <p>
+            {count} days from today is {date.toDateString()}
+          </p>
+        )}
+      </div>
+      <div>
+        <button onClick={handleReset}>reset</button>
+      </div>
     </div>
   );
 }
